f9009160d497f8cd940d29db360665b62b1d6049
maryamsaeedi17 <m.saeedi64@gmail.com>
2023-04-29 18:19:54 +0430

    histogram

---
+++ 6th OpenCV Python Codes/1-calculate-histogram.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+image=cv2.imread("Input/anoushe_ansari.jpg", cv2.IMREAD_GRAYSCALE)
+rows, cols=image.shape
+color_number=[]
+histogram=[]
+c=0
+for k in range(256):
+    for i in range(rows):
+        for j in range(cols):
+            if image[i][j]==k:
+                c+=1
+    histogram.append(c)
+    c=0
+    color_number.append(k)
+
+#plt.plot(histogram)
+#plt.hist(histogram, 30)
+plt.bar(color_number ,histogram)
+plt.show()
+            
---
+++ 6th OpenCV Python Codes/Readme.md
@@ -0,0 +1,12 @@
+# 1- Calculate the histogram of an image and shoe on diagram.
+
+## Input:
+![anoushe](Input/anoushe_ansari.jpg)
+
+## Outputs:
+
+![Figure_1](Output\Figure_1.jpeg)
+
+![Figure_2](Output\Figure_2.jpeg)
+
+![Figure_3](Output\Figure_3.jpeg)
